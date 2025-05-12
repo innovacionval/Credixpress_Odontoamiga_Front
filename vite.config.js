f138ec4fb276a66a585b16f807abc9ab3cd59198
Jorge Castillo <60049228+jmcc153@users.noreply.github.com>
2025-05-12 17:30:23 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/",
-  };
-  if (command != "serve") {
-    config.base = "/Credixpress_Odontoamiga_Front/";
-    //config.base = "/";
-  }
-  return config;
-});
+export default defineConfig({
+   plugins: [react()],
++  base: 'Credixpress_Odontoamiga_Front/'
+})
 

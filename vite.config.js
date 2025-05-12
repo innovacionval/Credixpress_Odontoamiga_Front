cd654da076f7494d3aa836454fa141dece0fa11d
Jorge Castillo <jmcastillo9915@gmail.com>
2025-05-12 11:22:00 -0500

    [fix] vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:"https://github.com/innovacionval/Credixpress_Odontoamiga_Front"
-})
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+  if (command != "serve") {
+    config.base = "/odontoamiga/";
+    //config.base = "/";
+  }
+  return config;
+});
+

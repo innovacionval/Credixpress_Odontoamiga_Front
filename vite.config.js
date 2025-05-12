451150afe817f3c7c66f8a2377b52451b8d184d3
Jorge Castillo <jmcastillo9915@gmail.com>
2025-05-12 10:59:42 -0500

    [Fix] deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"https://github.com/innovacionval/Credixpress_Odontoamiga_Front"
 })

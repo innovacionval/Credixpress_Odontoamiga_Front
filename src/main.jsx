cab877b2e9fa8e2ad3aefcca6879bc77ee531d4f
Jorge Castillo <jmcastillo9915@gmail.com>
2025-05-12 11:47:22 -0500

    [Modify] router dom

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename={import.meta.env.DEV ? '/' : '/Credixpress_Odontoamiga_Front/'}>
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,

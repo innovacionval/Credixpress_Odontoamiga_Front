8fada5b99c50d5cd1f435c08dfc38d609c35fd82
Jorge Castillo <jmcastillo9915@gmail.com>
2025-05-13 15:00:56 -0500

    [Add] logic function to simulator page

---
+++ src/utils/index.js
@@ -0,0 +1,26 @@
+export const functionToMoney = (value) => {
+  if (!value) return "0";
+  return new Intl.NumberFormat("es-CO", {
+    style: "currency",
+    currency: "COP",
+    minimumFractionDigits: 0,
+  }).format(value);
+};
+
+export const moneyToFunction = (value) => {
+  if (!value) return 0;
+  return Number(value.replace(/\./g, "").replace(/\$/g, ""));
+};
+
+export const dateSimulator = (date, day, quotes = 0, endDate = false) => {
+  if (!date) return "";
+  const newDate = new Date(date);
+  if (!endDate) {
+    newDate.setMonth(newDate.getMonth() + 1);
+  }else {
+    newDate.setMonth(newDate.getMonth() + (quotes + 1));
+  }
+  newDate.setDate(day);
+
+  return newDate.toISOString().split("T")[0];
+};

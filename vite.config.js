8daca6f406b82885b3d23d2197055cde0149090b
Jorge Castillo <jmcastillo9915@gmail.com>
2025-05-21 23:42:48 -0500

    [Fix] deploy test

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  return {
-    base: env.VITE_BASE_URL || '/',
-  };
-});
+export default defineConfig({
+   plugins: [react()],
+   base: '/Credixpress_Odontoamiga_Front/'
+})
 

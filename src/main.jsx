51dd6143ebb6b9e0d091381b7ebb3d7553b699ac
Jorge Castillo <jmcastillo9915@gmail.com>
2025-05-21 22:56:18 -0500

    [Modify] url test

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/Credixpress_Odontoamiga_Front/'}>
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/Credixpress_Odontoamiga_Front_test/'}>
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
    plugins: [react()],
-   base: '/Credixpress_Odontoamiga_Front/'
+   base: '/Credixpress_Odontoamiga_Front_test/'
 })
 
